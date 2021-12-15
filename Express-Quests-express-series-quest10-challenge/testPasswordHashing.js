56003c02abd72554c7f21468886ca5966259465c
MariaPilarRuizColas <mariapilar.ruizcolas@neuf.fr>
2021-12-15 21:58:08 +0100

    Challenge

---
+++ Express-Quests-express-series-quest10-challenge/testPasswordHashing.js
@@ -0,0 +1,22 @@
+/*const User = require('./models/user');
+
+
+User.hashPassword('myPlainPassword').then((hashedPassword) => {
+
+  console.log(hashedPassword);
+
+});*/
+const User = require('./models/user');
+
+
+User.verifyPassword(
+
+ 'myPlainPassword',
+
+ '$argon2id$v=19$m=65536,t=5,p=1$6F4WFjpSx9bSq9k4lp2fiQ$cjVgCHF/voka5bZI9YAainiaT+LkaQxfNN638b/h4fQ'
+
+).then((passwordIsCorrect) => {
+
+  console.log(passwordIsCorrect);
+
+});
